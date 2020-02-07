8abc7cc5fff9c4a6c008f28aca34cea63948e0f4
FraDS18 <fradisario18@gmail.com>
2020-02-07 23:12:02 +0100

    carico frontend

--- src/app/constants/constant.ts
+++ src/app/constants/constant.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://sini-doneit-backend.herokuapp.com'
-export const WS_URL = 'ws://sini-doneit-backend.herokuapp.com'
+export const WS_URL = 'wss://sini-doneit-backend.herokuapp.com'
